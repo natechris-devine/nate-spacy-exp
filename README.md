d22d2096f0f1c2be5a59798b773796eb4fe1e2fc
NateChris <divineconclusion@gmail.com>
2020-06-02 01:31:11 -0500

    Basic title finder and date finder added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Resumate Experiments with Spacy
+
+## Title Finder - Basic
+Designed to extract title from sentences in the form '...{Degree Type} in {Subject}...' using simple keyword matching
--- titleFinder.py
+++ titleFinder.py
@@ -1,27 +1,61 @@
 import spacy
-from spacy import displacy
 
-text = "I have a Masters in User Experience Design from Stanford. I also got a BSc in Software Engineering"
-text = "I went to Stanford and got a Masters Degree."
-text = "I got my AWS Cert two years ago."
-text = "I have my Bachelors in Economics and my Masters in Social Sciences."
-text = "My masters degree was in Economics and I got it in two years"
+from pprint import pprint
+from utils import *
 
-"""
-Task List:
-1) 
-"""
+# kinda lazy: don't want to have to run multiple files. Hence the import of dateFinder here
+from dateFinder import dateFinder_basic
+
 
 nlp = spacy.load("en_core_web_sm")
 
-doc = nlp(text)
+# text = "I have a Masters in User Experience Design from Stanford. I also got a BSc in Software Engineering"
+# text = "I went to Stanford and got a Masters Degree."
+# text = "I got my AWS Cert two years ago."
+# text = "I have my Bachelors in Economics and my Masters in Social Sciences."
+text = "My masters degree was in Economics and I got it in two years"
 
-def noun_chunks(doc):
-    chunks = []
-    for chunk in doc.noun_chunks:
-        chunks.append((chunk.text, chunk.root.text, chunk.root.dep_,
-            chunk.root.head.text))
-    return chunks
+# Target text format for basic title finder
+text_basic = ["I have a Masters in User Experience Design from Stanford",
+              "I have a BSc in Software Engineering",
+              "I have a PhD in Psychology",
+              "I have an Associate Degree in Web Design",
+              "I have a Masters Degree in Web Design and a Bachelors in Computer Science"
+]
+
+# The last one is an experiment to consider later
+
+title_words = ["masters", "bsc", "bachelors", "degree", "doctorate", "phd", "associate"]
+
+"""
+Task List - Basic Title Finder:
+1) Only degree titles. Extract from sentences in the form ...{Degree Title} in {Subject}.
+"""
+
+doc = nlp(text_basic[0])
+
+
+def titleFinder_basic(doc):
+    
+    # title_words = ["masters", "bsc", "bachelors", "associate", "doctorate", "phd"]
+    title = []
+    watch = 0
+
+    for token in doc:
+        if token.lemma_.lower() in title_words:
+            watch = 1
+            title.append(token)
+            # Consideration here could be to skip the work 'degree' from adding to this list
+        elif watch == 1 and token.dep_ == "prep":
+            watch = 2
+        elif watch == 2 and token.dep_ in ["compound", "pobj"]:
+            title.append(token)
+        else:
+            watch = 0
+
+    return title
+        
+
 
 def titleFinder(doc):
     """Returns a list of tuples, in the form (title: spacy.doc.token, toQuery: Bool). - Idea, could also be a dict
@@ -55,30 +89,7 @@ def titleFinder(doc):
     return titles
 
 
-def to_tree(token):
-    """Returns a flat list of the children of a token"""
-    children = list(token.children)
-    if children == []:
-        return token.text
-    else:
-        result = [token.text]
-        result.append(list(map(to_tree, children)))
-        return result
-
-print(to_tree(doc[2]))
 
 
-def dependencyDiagram(doc):
-    """not working yet"""
-    displacy.serve(doc, style='dep')
-
-# print(noun_chunks(doc))
-# titleFinder(doc)
 
 
-"""Scrap Code"""
-'''
-print(token.text)
-    print([t.text for t in token.subtree])
-    # lst = []
-'''
